2bdbf6fb2004dec18d4b680c0cb70dbe80363a6f
Munawwar1008 <munawar.shaikh10@gmail.com>
2022-04-25 12:12:35 +0530

    new

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
 // Store the wallet amount to your local storage with key "amount"
+let counts = setInterval(updated);
+  let upto =0 
+
+
+function updated(){
+  let amount = document.getElementById("amount").value;
+
+  let count = document.getElementById("wallet")
+  count.innerHTML = amount
+
+ 
+  localStorage.setItem("wallet",JSON.stringify(amount))
+ 
+}
